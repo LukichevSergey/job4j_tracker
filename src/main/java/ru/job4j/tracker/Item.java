d1aaab9c87261848bda4e1bf93f224e82d509123
Lukichev_s <Lukichev_s@mail.ru>
2023-08-13 20:31:08 +0700

    8. Date. Отображение даты. [#310017 #54175]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,12 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() { }
 
@@ -14,6 +18,11 @@ public class Item {
         this.id = id;
         this.name = name;
     }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
 
     public int getId() {
         return id;
