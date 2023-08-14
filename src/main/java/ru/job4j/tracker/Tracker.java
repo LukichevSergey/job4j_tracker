3f1736203feff3d86d43b5336cc16e0847e97072
Lukichev_s <Lukichev_s@mail.ru>
2023-08-14 19:57:03 +0700

    7. Метод удаления заявки Tracker.delete [#211749 #54113]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -56,6 +56,20 @@ public class Tracker {
         return false;
     }
 
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            items[index] = null;
+            int start = index + 1;
+            int length = size - index - 1;
+            System.arraycopy(items, start, items, index, length);
+            items[size - 1] = null;
+            size--;
+            return true;
+        }
+        return false;
+    }
+
     private int indexOf(int id) {
         int rsl = -1;
         for (int index = 0; index < size; index++) {
